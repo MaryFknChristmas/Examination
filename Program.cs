1baef6e1b66db676a9497dd3edf1dd15f962d269
Marina <gtellier.mecanics@gmail.com>
2022-08-05 12:52:06 +0300

    Code created

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string[] array = {"Russia", "Denmark", "Kazan"};
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
